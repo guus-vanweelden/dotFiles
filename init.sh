919f465754fd0f501747e079d5c8fa117c3cf9d4
Guus van Weelden <vanweelden@veeseo.com>
2016-01-10 16:00:13 +0000

    update init script

--- init.sh
+++ init.sh
@@ -5,33 +5,30 @@
 
 # update & upgrade (ubuntu)
 sudo apt-get update
-sudo apt-get dist-upgrade
-sudo apt-get  -y install git git-svn subvesion mercurial vim-nox
+sudo apt-get -y dist-upgrade
+sudo apt-get -y install git git-svn subversion mercurial vim-nox build-essential
 
 mkdir -p ~/dev
 cd ~/dev
 
 # build and install tmux
-git clone git@github.com:tmux/tmux.git
+git clone https://github.com/tmux/tmux.git
 cd ~/dev/tmux/
-sudo apt-get -y install libncurses-dev pkg-config gcc libevent-dev 
+sudo apt-get -y install libncurses-dev pkg-config gcc libevent-dev autoconf
 autoreconf -i --force && ./configure
 sudo make install
 cd ~/dev/
 
 # build and install fish-shell
-git clone git://github.com/fish-shell/fish-shell.git
+git clone https://github.com/fish-shell/fish-shell.git
 cd ~/dev/fish-shell/
 autoconf
 ./configure
 make
 sudo make install
 
-# get dotFiles & link them
-cd ~/dev
-git clone https://gitlab.com/g.van.weelden/dotFiles.git
-
 # install vim extensions
+cd ~
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
 sh ./install.sh
 
@@ -40,3 +37,11 @@ mkdir -p ~/go
 cd /tmp/
 wget https://storage.googleapis.com/golang/go1.5.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.5.2.linux-amd64.tar.gz
+
+# get dotFiles & link them
+cd ~/dev
+git clone https://gitlab.com/g.van.weelden/dotFiles.git
+ln -s ~/dev/dotFiles/git/gitconfig ~/.gitconfig
+rm -rf ~/.config/fish/
+ln -s ~/dev/dotFiles/fish .config/fish
+git remote set-url origin git@gitlab.com:g.van.weelden/dotFiles.git
