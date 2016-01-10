50bef4f2c64f85c25af80d8e934378e33df62e79
Guus van Weelden <vanweelden@veeseo.com>
2016-01-10 15:11:39 +0000

    add -y flag to apt-get

--- init.sh
+++ init.sh
@@ -6,15 +6,15 @@
 # update & upgrade (ubuntu)
 sudo apt-get update
 sudo apt-get dist-upgrade
-sudo apt-get install git git-svn subvesion mercurial vim-nox
+sudo apt-get  -y install git git-svn subvesion mercurial vim-nox
 
-mkdir dev
-cd dev
+mkdir -p ~/dev
+cd ~/dev
 
 # build and install tmux
 git clone git@github.com:tmux/tmux.git
 cd ~/dev/tmux/
-sudo apt-get install libncurses-dev pkg-config gcc libevent-dev 
+sudo apt-get -y install libncurses-dev pkg-config gcc libevent-dev 
 autoreconf -i --force && ./configure
 sudo make install
 cd ~/dev/
@@ -28,13 +28,15 @@ make
 sudo make install
 
 # get dotFiles & link them
+cd ~/dev
+git clone https://gitlab.com/g.van.weelden/dotFiles.git
 
 # install vim extensions
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
 sh ./install.sh
 
 # install go1.5.2
+mkdir -p ~/go
 cd /tmp/
 wget https://storage.googleapis.com/golang/go1.5.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.5.2.linux-amd64.tar.gz
-git clone https://gitlab.com/g.van.weelden/dotFiles.git
